6e4d9116351a7fe1e07f2e19deedab3194f5750f
Erdos06 <erdoskuanyshbek@gmail.com>
2025-10-19 03:55:35 +0500

    change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = 'https://zaman-back.vercel.app/api'
 
 // Функция для получения userId из localStorage
 const getUserId = () => {
